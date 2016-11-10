9396e47457f1cd2f58a76719c8f332a135df8033
Frank Bardon Jr <frankbardon@gmail.com>
2016-11-10 16:55:21 -0500

    Created base generic exception

---
+++ src/Exception/GenericException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Webmax\VelocifyClient\Exception;
+
+use RuntimeException;
+
+/**
+ * Generic Velocify client exception
+ *
+ * @author Frank Bardon Jr. <frankbardon@gmail.com>
+ */
+class GenericException extends RuntimeException implements VelocifyClient
+{
+}
